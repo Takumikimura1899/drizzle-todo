a27495e580d8ac08b82009fe8b177b60e2c8c9c6
Takumikimura1899 <kimura141899@gmail.com>
2024-05-22 21:47:11 +0900

    about table button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,45 @@
 import { getTodos } from '@/actions/todoActions';
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 export default async function Home() {
   const todos = await getTodos();
   return (
     <main>
       <h1 className='text-center text-3xl'>drizzle todo</h1>
-      {todos.map((todo) => (
-        <div key={todo.id} className=''>
-          <span>{todo.title}</span>
-          <span>{todo.status}</span>
-        </div>
-      ))}
+      <Table>
+        <TableCaption>Todo List</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>タイトル</TableHead>
+            <TableHead>ステータス</TableHead>
+            <TableHead>作成日</TableHead>
+            <TableHead>更新日</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {todos.map((todo) => (
+            <TableRow key={todo.id}>
+              <TableCell>{todo.title}</TableCell>
+              <TableCell>{todo.status}</TableCell>
+              <TableCell>{todo.createdAt.toDateString()}</TableCell>
+              <TableCell>{todo.updatedAt.toDateString()}</TableCell>
+              <TableCell className='w-1/6 space-x-2'>
+                <Button variant='outline'>編集</Button>
+                <Button variant='destructive'>削除</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </main>
   );
 }
